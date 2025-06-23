0c4f9c2258c1ccc297b2771c4e536fbf667033a9
Miguel Santiago Gómez Suárez <Hitomiblood@gmail.com>
2025-06-22 20:38:05 -0500

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 ssh -o StrictHostKeyChecking=no azureuser@20.29.47.112 <<HTML
 
-    cd \home\azureuser\PracticasGA
+    cd /home/azureuser/PracticasGA
 
     git pull --rebase origin main
-    
+
 HTML
